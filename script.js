4c52d64bf8072c9f61a7e22dbeb266142d76af46
Charlie Parker <129253904+NotKilluaZ@users.noreply.github.com>
2023-07-24 18:49:41 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,180 @@
+let code = parseInt(window.prompt("Enter Your Secret Code:"));
+
+if (code === 000) {
+  codeMessage = document.getElementById("welcome");
+  codeMessage.style.display = "block";
+} else if (code === 801) {
+  codeMessage = document.getElementById("badDay");
+  codeMessage.style.display = "block";
+} else if (code === 929) {
+  codeMessage = document.getElementById("badDay1");
+  codeMessage.style.display = "block";
+} else if (code === 494) {
+  codeMessage = document.getElementById("badDay2");
+  codeMessage.style.display = "block";
+} else if (code === 143) {
+  codeMessage = document.getElementById("missMe");
+  codeMessage.style.display = "block";
+}  else if (code === 216) {
+  codeMessage = document.getElementById("missMe1");
+  codeMessage.style.display = "block";
+} else if (code === 720) {
+  codeMessage = document.getElementById("missMe2");
+  codeMessage.style.display = "block";
+} else if (code === 391) {
+  codeMessage = document.getElementById("sleep");
+  codeMessage.style.display = "block";
+} else if (code === 226) {
+  codeMessage = document.getElementById("sleep1");
+  codeMessage.style.display = "block";
+} else if (code === 332) {
+  codeMessage = document.getElementById("sleep2");
+  codeMessage.style.display = "block";
+} else if (code === 504) {
+  codeMessage = document.getElementById("insecure");
+  codeMessage.style.display = "block";
+} else if (code === 234) {
+  codeMessage = document.getElementById("insecure1");
+  codeMessage.style.display = "block";
+} else if (code === 088) {
+  codeMessage = document.getElementById("insecure2");
+  codeMessage.style.display = "block";
+} else if (code === 972) {
+  codeMessage = document.getElementById("lonely");
+  codeMessage.style.display = "block";
+} else if (code === 721) {
+  codeMessage = document.getElementById("lonely1");
+  codeMessage.style.display = "block";
+} else if (code === 485) {
+  codeMessage = document.getElementById("worried");
+  codeMessage.style.display = "block";
+} else if (code === 500) {
+  codeMessage = document.getElementById("mindOff");
+  codeMessage.style.display = "block";
+} else if (code === 001) {
+  codeMessage = document.getElementById("suicide");
+  codeMessage.style.display = "block";
+} else if (code === 999) {
+  codeMessage = document.getElementById("suicide1");
+  codeMessage.style.display = "block";
+} else if (code === 680) {
+  codeMessage = document.getElementById("workProblems");
+  codeMessage.style.display = "block";
+} else if (code === 735) {
+  codeMessage = document.getElementById("sick");
+  codeMessage.style.display = "block";
+} else if (code === 052) {
+  codeMessage = document.getElementById("sick1");
+  codeMessage.style.display = "block";
+} else if (code === 016) {
+  codeMessage = document.getElementById("overthinking");
+  codeMessage.style.display = "block";
+} else if (code === 200) {
+  codeMessage = document.getElementById("overthinking1");
+  codeMessage.style.display = "block";
+} else if (code === 111) {
+  codeMessage = document.getElementById("overthinking2");
+  codeMessage.style.display = "block";
+} else if (code === 136) {
+  codeMessage = document.getElementById("hug");
+  codeMessage.style.display = "block";
+} else if (code === 103) {
+  codeMessage = document.getElementById("hug1");
+  codeMessage.style.display = "block";
+} else if (code === 888) {
+  codeMessage = document.getElementById("comp");
+  codeMessage.style.display = "block";
+} else if (code === 816) {
+  codeMessage = document.getElementById("comp1");
+  codeMessage.style.display = "block";
+} else if (code === 801) {
+  codeMessage = document.getElementById("comp2");
+  codeMessage.style.display = "block";
+} else if (code === 808) {
+  codeMessage = document.getElementById("comp3");
+  codeMessage.style.display = "block";
+} else if (code === 829) {
+  codeMessage = document.getElementById("comp4");
+  codeMessage.style.display = "block";
+} else if (code === 836) {
+  codeMessage = document.getElementById("comp5");
+  codeMessage.style.display = "block";
+} else if (code === 821) {
+  codeMessage = document.getElementById("comp6");
+  codeMessage.style.display = "block";
+} else if (code === 667) {
+  codeMessage = document.getElementById("mistake");
+  codeMessage.style.display = "block";
+} else if (code === 042) {
+  codeMessage = document.getElementById("motive");
+  codeMessage.style.display = "block";
+} else if (code === 508) {
+  codeMessage = document.getElementById("motive1");
+  codeMessage.style.display = "block";
+} else if (code === 118) {
+  codeMessage = document.getElementById("bored");
+  codeMessage.style.display = "block";
+} else if (code === 954) {
+  codeMessage = document.getElementById("tiredPeople");
+  codeMessage.style.display = "block";
+} else if (code === 777) {
+  codeMessage = document.getElementById("tiredPeople1");
+  codeMessage.style.display = "block";
+} else if (code === 593) {
+  codeMessage = document.getElementById("mad");
+  codeMessage.style.display = "block";
+} else if (code === 427) {
+  codeMessage = document.getElementById("mad1");
+  codeMessage.style.display = "block";
+} else if (code === 960) {
+  codeMessage = document.getElementById("memory");
+  codeMessage.style.display = "block";
+} else if (code === 961) {
+  codeMessage = document.getElementById("memory1");
+  codeMessage.style.display = "block";
+} else if (code === 962) {
+  codeMessage = document.getElementById("memory2");
+  codeMessage.style.display = "block";
+} else if (code === 963) {
+  codeMessage = document.getElementById("memory3");
+  codeMessage.style.display = "block";
+} else if (code === 964) {
+  codeMessage = document.getElementById("memory4");
+  codeMessage.style.display = "block";
+} else if (code === 965) {
+  codeMessage = document.getElementById("memory5");
+  codeMessage.style.display = "block";
+} else if (code === 351) {
+  codeMessage = document.getElementById("noTalk");
+  codeMessage.style.display = "block";
+} else if (code === 468) {
+  codeMessage = document.getElementById("hateYou");
+  codeMessage.style.display = "block";
+} else if (code === 008) {
+  codeMessage = document.getElementById("cry");
+  codeMessage.style.display = "block";
+} else if (code === 055) {
+  codeMessage = document.getElementById("proud");
+  codeMessage.style.display = "block";
+} else if (code === 440) {
+  codeMessage = document.getElementById("lost");
+  codeMessage.style.display = "block";
+} else if (code === 120) {
+  codeMessage = document.getElementById("wish");
+  codeMessage.style.display = "block";
+} else if (code === 130) {
+  codeMessage = document.getElementById("wish1");
+  codeMessage.style.display = "block";
+} else if (code === 140) {
+  codeMessage = document.getElementById("wish2");
+  codeMessage.style.display = "block";
+} else if (code === 150) {
+  codeMessage = document.getElementById("wish3");
+  codeMessage.style.display = "block";
+} else if (code === 038) {
+  codeMessage = document.getElementById("loveYou");
+  codeMessage.style.display = "block";
+} else {
+  codeMessage = document.getElementById("error");
+  codeMessage.style.display = "block";
+}
